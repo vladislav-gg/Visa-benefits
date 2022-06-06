5a170b630d03083a07d4ac32fce05e232de8f78c
Vladislav <selies@mail.ru>
2022-06-06 23:28:55 +0100

    added tyniMC html text editor

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const VisaTask = require("./models/visa");
 
 const password = process.env.MONGODB_PASSWORD;
 const uri = `mongodb+srv://klay:${password}@wikiapi.dd5mg.mongodb.net/VisaDB?retryWrites=true&w=majority`;
-mongoose.connect(uri).then(() => {
-	console.log("connected to db!");
-});
+mongoose
+	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("connected to db!");
+	});
 
 app.set("view engine", "ejs");
 
@@ -20,55 +22,57 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/static", express.static("public"));
 
-// GET METHOD
-app.get("/", (req, res) => {
-	VisaTask.find({}, (err, tasks) => {
-		res.render("benefits.ejs", { visaTask: tasks });
-	});
-});
+app.use(require("./routes/visaList"));
 
-// Post motehod
-app.post("/", async (req, res) => {
-	const visaTask = new VisaTask({
-		title: req.body.title,
-		content: req.body.content,
-	});
+// // GET METHOD
+// app.get("/", (req, res) => {
+// 	VisaTask.find({}, (err, tasks) => {
+// 		res.render("benefits.ejs", { visaTask: tasks });
+// 	});
+// });
 
-	try {
-		await visaTask.save();
-		console.log("success");
-		res.redirect("/");
-	} catch (err) {
-		console.log(err);
-		res.redirect("/");
-	}
-});
+// // Post motehod
+// app.post("/", async (req, res) => {
+// 	const visaTask = new VisaTask({
+// 		title: req.body.title,
+// 		content: req.body.content,
+// 	});
 
-// UPDATE METHOD
-app
-	.route("/edit/:id")
-	.get((req, res) => {
-		const id = req.params.id;
-		VisaTask.find({}, (err, tasks) => {
-			res.render("benefitsEdit.ejs", { visaTask: tasks, id: id });
-		});
-	})
-	.post((req, res) => {
-		const id = req.params.id;
-		VisaTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
-			if (err) return res.send(500, err);
-			res.redirect("/");
-		});
-	});
+// 	try {
+// 		await visaTask.save();
+// 		console.log("success");
+// 		res.redirect("/");
+// 	} catch (err) {
+// 		console.log(err);
+// 		res.redirect("/");
+// 	}
+// });
 
-// REMOVE METHOD
-app.route("/remove/:id").get((req, res) => {
-	const id = req.params.id;
-	VisaTask.findByIdAndRemove(id, (err) => {
-		if (err) return res.send(500, err);
-		res.redirect("/");
-	});
-});
+// // UPDATE METHOD
+// app
+// 	.route("/edit/:id")
+// 	.get((req, res) => {
+// 		const id = req.params.id;
+// 		VisaTask.find({}, (err, tasks) => {
+// 			res.render("benefitsEdit.ejs", { visaTask: tasks, id: id });
+// 		});
+// 	})
+// 	.post((req, res) => {
+// 		const id = req.params.id;
+// 		VisaTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+// 			if (err) return res.send(500, err);
+// 			res.redirect("/");
+// 		});
+// 	});
+
+// // REMOVE METHOD
+// app.route("/remove/:id").get((req, res) => {
+// 	const id = req.params.id;
+// 	VisaTask.findByIdAndRemove(id, (err) => {
+// 		if (err) return res.send(500, err);
+// 		res.redirect("/");
+// 	});
+// });
 
 app.listen(port, () => {
 	console.log("The server is running");
