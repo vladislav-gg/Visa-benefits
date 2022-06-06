c0a0aaa6169e984122bb7102b1e928c35cb66afb
Vladislav <selies@mail.ru>
2022-06-06 23:28:21 +0100

    added routes and images folders

---
+++ routes/visaList.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+
+const VisaTaskModel = require("../models/visa");
+
+router
+	.get("/", (req, res) => {
+		VisaTaskModel.find({}, (err, task) => {
+			res.render("benefits", { visaTask: task });
+		});
+	})
+	.post("/add/task", (req, res) => {
+		const { task } = req.body;
+		const newTask = new VisaTaskModel({
+			title: req.body.title,
+			content: req.body.content,
+		});
+		newTask
+			.save()
+			.then(() => {
+				console.log("added");
+				res.redirect("/");
+			})
+			.catch((err) => console.log(err));
+	})
+	.get("/delete/task/:_id", (req, res) => {
+		const { _id } = req.params;
+		VisaTaskModel.deleteOne({ _id })
+			.then(() => {
+				console.log("deleted");
+				res.redirect("/");
+			})
+			.catch((err) => console.log(err));
+	});
+// .get('/update/task/:_id', (req,res)=> {
+//     const {id} = req.params
+//     const info = VisaTaskModel.find()
+//     VisaTaskModel.updateOne({_id}, )
+// })
+
+module.exports = router;
